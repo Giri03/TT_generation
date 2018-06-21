310938604893ff9369ad8dd856ba65a140005d43
Giri03 <girisha.navani97@gmail.com>
2018-06-21 18:19:11 +0530

    added

---
+++ try2.py
@@ -0,0 +1,10 @@
+# from collections import Counter
+
+# u = [['lol', 'men', 'lol', 'donkey', 'donkey', 'lol', 'men', 'lol', 'lol', 'men', 'donkey'],['lol', 'men', 'lol', 'donkey', 'donkey', 'men', 'donkey'],['lol', 'men', 'lol', 'donkey', 'donkey', 'lol', 'men']]
+
+# for k, v in Counter(u).items():
+#     if v > 1:
+#         print(k)
+
+import random
+print(random.random())
