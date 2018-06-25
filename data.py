d187fb6216ef3b311cac3f8b9f52aa9ffe69c8bc
Giri03 <girisha.navani97@gmail.com>
2018-06-25 13:08:03 +0530

    new data n main files

--- data.py
+++ data.py
@@ -1,50 +1,63 @@
-from collections import Counter
-import random
-import sys
-# course [name, no_students, instructors]
+years = ['y2', 'y3', 'y4']
 
+y2 = [['s21', 's22', 's23', 's24', 's25'], ['l21','l22','l23']]
+y3 = [['s31', 's32', 's33', 's34', 's35', 's36'], ['l31','l32','l33','l34']]
+y4 = [['s41', 's42', 's43', 's44'], ['l41','l42','l43','l44']]
 
-c1 = ['c1', 25, ['James', 'Mike']]
-c2 = ['c2', 35, ['James', 'Mike', 'Steve']]
-c3 = ['c3', 25, ['James', 'Mike']]
-c4 = ['c4', 30, ['Jane', 'Steve']]
-c5 = ['c5', 35, ['Jane']]
-c6 = ['c6', 45, ['James', 'Steve']]
-c7 = ['c7', 45, ['Jane', 'Mike']]
+# t-->teachers 
+# ti (id, timeslot)
+# t1 = ['t1', 'ts1']
+# t2 = ['t2', 'ts1']
+# t3 = ['t3', 'ts1']
+# t4 = ['t4', 'ts2']
+# t7 = ['t7', 'ts2']
+# t8 = ['t8', 'ts2']
+# t9 = ['t9', 'ts1']
+# t10 = ['t10', 'ts2']
+# t11 = ['t11', 'ts2']
+# t12 = ['t12', 'ts1']
+# t13 = ['t13', 'ts1'] 
+# t14 = ['t14', 'ts2']
+# t15 = ['t15', 'ts1']
+# t16 = ['t16', 'ts2']
+# t17 = ['t17', 'ts2']
+# t18 = ['t18', 'ts1']
+# t19 = ['t19', 'ts1']
+# t20 = ['t20', 'ts2']
 
-course = [c1, c2, c3, c4, c5, c6, c7]
+# subjects
+s21 = ['s21', [['t1', 'AB']]]
+s22 = ['s22', [['t2', 'AB']]]
+s23 = ['s23', [['t3', 'AB']]]
+s24 = ['s24', [['t4', 'A'], ['t5', 'B']]]
+s25 = ['s25', [['t6', 'AB']]]
+s31 = ['s31', [['t7', 'AB']]]
+s32 = ['s32', [['t8', 'AB']]]
+s33 = ['s33', [['t9', 'A'], ['t10', 'B']]]
+s34 = ['s34', [['t11', 'AB']]]
+s35 = ['s35', [['t12', 'AB']]]
+s36 = ['s36', [['t13', 'A'], ['t14', 'B']]]
+s41 = ['s41', [['t15', 'A'], ['t16', 'B']]]
+s42 = ['s42', [['t17', 'AB']]]
+s43 = ['s43', [['t18', 'AB']]]
+s44 = ['s44', [['t19', 'A'], ['t20', 'B']]]
 
-# rooms  [id, capacity]
-r1 = ['r1', 25]
-r2 = ['r2', 45]
-r3 = ['r3', 35]
+#labs
+l21 = ['l21', ['t2', 't9', 't13']]
+l22 = ['l22', ['t4', 't5']]
+l23 = ['l23', ['t6', 't19']]
+l31 = ['l31', ['t3', 't8', 't7']]
+l32 = ['l32', ['t9', 't10', 't13']]
+l33 = ['l33', ['t4', 't7', 't12', 't18', 't20']]
+l34 = ['l34', ['t13', 't14']]
+l41 = ['l41', ['t16']]
+l42 = ['l42', ['t17', 't10']]
+l43 = ['l43', ['t5', 't12', 't14', 't18']]
+l44 = ['l44', ['t19', 't20']]
 
-rooms = [r1, r2, r3]
+subjects = [s21, s22, s23, s24, s25, s31, s32, s33, s34, s35, s36, s41, s42, s43, s44]
+labs = [l21, l22, l23, l31, l32, l33, l34, l41, l42, l43, l44]
 
-# instructor [id, name]
-i1 = ['i1', 'James']
-i2 = ['i2', 'Mike']
-i3 = ['i3', 'Steve']
-i4 = ['i4', 'Jane']
-
-instructor = [i1, i2, i3, i4]
-
-# meetingtime [id, time]
-# m1 = ['m1', '9am-10am']
-# m2 = ['m2', '10am-11am']
-# m3 = ['m3', '11am-12pm']
-# m4 = ['m4', '12pm-1pm']
-
-meettime = ['m1', 'm2', 'm3', 'm4']
-
-# departments
-math = [c1, c3]
-ee = [c2, c4, c5]
-phy = [c6, c7]
-
-dept = [math, ee, phy]
-data = [dept, instructor, course, rooms, meettime]
+meettime = [['m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7'],['m21', 'm22', 'm23', 'm24']]
 days = ['mon', 'tues', 'wed', 'thur', 'fri']
-
-
-            
+rooms = ['r1', 'r2', 'r3', 'r4', 'r5', 'r6']
---
+++ mains.py
@@ -0,0 +1,123 @@
+from data import course, days, meettime
+from collections import Counter
+import random
+import sys
+
+population_size = 200
+
+def create_population():
+    population = []
+    l = 0
+    population_size = 200
+    for j in range(population_size):
+        for i in course:
+            population.append([l , i[0], random.choice(i[2]), random.choice(days), random.choice(meettime), 0])
+            l = l + 1
+    return population
+
+def fitness(population):
+    count = 0
+    for i in days:
+        for j in meettime:
+            cou = Counter(population[s][2] for s in range(len(population)) if population[s][4] == j and population[s][3] == i )
+
+    # extracting from counter dictionary                  
+    for i in population:
+        for j,k in cou.items():
+            if j == i[2]:
+                i[5] = k 
+    return population
+
+def tournament(population):
+    tournment_size = 350
+    # some max no.
+    cross = []
+    # algo
+    # iterate 200 time
+    # for each iteration choose 350 random elements from overall population
+    population1 = []
+    for i in range(population_size):
+        min = sys.maxsize
+        for j in range(tournment_size):
+            cross = random.choice(population)
+            # print(len(cross))
+            if(min > cross[5]):
+                min = cross[5]
+        population1.append(cross)
+    return population1
+
+def swap(a, b):
+    temp = a
+    a = b
+    b = temp
+    return a, b
+
+def crossover(population1):
+    pc = 0.7
+    # here selection is list obtained after tournament selection
+    for i in range(0, 200, 2):
+        if random.random() > pc:
+            if(random.choice([True, False])):
+                swap(population1[i][4], population1[i+1][4])
+            if(random.choice([True, False])):
+                swap(population1[i][5], population1[i+1][5])
+    return population1
+
+def mutation(population1):
+    pm = 0.2
+    for i in range(0, 200, 2):
+        if random.random() < pm:
+            if(random.choice([True, False])):
+                swap(population1[i][4], random.choice(days))
+            if(random.choice([True, False])):
+                swap(population1[i][5], random.choice(meettime))
+    return population1
+
+def new_population(population, population1):
+    # create new population .. here range for new population should be 25% of original ..so 200 from crossover and mutation + 150
+    for i in range(150):
+        population1.append(random.choice(population))
+    population = population1
+    return population
+
+def timetables(population):
+    timetable = []
+    for i in days:
+        for j in meettime:
+            min = sys.maxsize
+            for s in range(len(population)):
+                table = population[s]
+                if(j==table[4] and i==table[3] and table[5]<min):
+                    min = table[5]
+                    remem = table
+            timetable.append(remem)
+    return timetable    
+
+def change_fitness(population):
+    for x in population:
+        x[5] = 0
+    return population
+
+if __name__ == '__main__':
+    population = create_population()
+    for i in range(100):
+        population = fitness(population)
+        population1 = tournament(population)
+        population1 = crossover(population1)
+        population1 = mutation(population1)
+        population1 = change_fitness(population1)
+        population1 = fitness(population1)
+        population = new_population(population, population1)
+        timetable = timetables(population)
+        timetable = change_fitness(timetable)
+        timetable = fitness(timetable)
+        
+    for u in timetable:
+        print(u)
+
+
+
+
+
+
+
