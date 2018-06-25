38d448f64ec71bdb759b9f6fe272961ed67dc116
Giri03 <girisha.navani97@gmail.com>
2018-06-25 15:00:03 +0530

    changes in main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datas import subjects, days, meettime, rooms, labs
+from data import subjects, days, meettime, rooms, labs
 from collections import Counter
 import random
 import sys
@@ -7,13 +7,24 @@ population_size = 200
 
 def create_population():
     population = []
+    population_sub = []
+    population_lab = []
     l = 0
     population_size = 200
-    
     for j in range(population_size):
-        for i in course:
-            population.append([l , i[0], random.choice(i[2]), random.choice(days), random.choice(meettime), 0])
-            l = l + 1
+        for i in subjects:
+            p = random.choice(i[1])
+            if(p[1] == 'AB'):
+                population_sub.append([i[0], p[0], random.choice(['A', 'B']), random.choice(rooms), random.choice(days), random.choice(meettime[0])])
+            else:
+                population_sub.append([i[0], p[0], p[1], random.choice(rooms), random.choice(days), random.choice(meettime[0])])    
+        for i in labs:
+            population_lab.append([i[0], random.choice(i[1]), random.choice(['A', 'B']), random.choice(days), random.choice(meettime[1])])
+    population = population_lab.append(population_sub)
     return population
 
+population = create_population()
+for c in population:
+    for j in c:
+        print(c)
 
