3162f45b1cc050194100d36bde4e03a6af4e493a
“aarti@12” <bhagtaniaarti@gmail.com>
2018-06-29 01:35:46 +0530

    add timetable

--- main.py
+++ main.py
@@ -1,71 +1,45 @@
-from data import subjects, days, meettime, rooms, labs, years, teachers
+from data import subjects, days, meettime, rooms, labs, years, teachers,divs
 from collections import Counter
 import random
 import sys
 
 population_size_sub = 100
 population_size_lab = 50
-<<<<<<< HEAD
-population = []
-=======
->>>>>>> 681e808d5312b55001a8966530a1a586fdef0e15
 
 def create_population():
-
+    population = []
     population_sub = []
     population_lab = []
     for j in range(population_size_sub):
         l = -1
-<<<<<<< HEAD
-        for k in subjects:
-            l += 1
-            for i in k:
-                p = random.choice(i[1])
-                if(p[1] == 'AB'):
-                    population_sub.append([years[l][0], i[0], p[0], random.choice(['A', 'B']), random.choice(rooms), random.choice(days), random.choice(meettime[0]), -1])
-                else:
-                    population_sub.append([years[l][0], i[0], p[0], p[1], random.choice(rooms), random.choice(days), random.choice(meettime[0]), -1])
-=======
         ids = 0
         for k in subjects:
             l += 1
             for i in k:
-                ids += 1 
+                ids += 1
                 p = random.choice(i[1])
                 if(p[1] == 'AB'):
                     population_sub.append([years[l][0], i[0], p[0], random.choice(['A', 'B']), random.choice(rooms), random.choice(days), random.choice(meettime[0]), ids,-1])
                 else:
-                    population_sub.append([years[l][0], i[0], p[0], p[1], random.choice(rooms), random.choice(days), random.choice(meettime[0]), ids, -1])    
->>>>>>> 681e808d5312b55001a8966530a1a586fdef0e15
+                    population_sub.append([years[l][0], i[0], p[0], p[1], random.choice(rooms), random.choice(days), random.choice(meettime[0]), ids, -1])
+    ids = 1000
     for j in range(population_size_lab):
         l = -1
         for k in labs:
             l += 1
             for i in k:
-<<<<<<< HEAD
-                p = random.choice(i[1])
-                if(p[1] == 'AB'):
-                    population_lab.append([years[l][0], i[0], p[0], random.choice(['A', 'B']), random.choice(days), random.choice(meettime[1]), -1])
-                else:
-                    population_lab.append([years[l][0], i[0], p[0], p[1], random.choice(days), random.choice(meettime[1]), -1])
-=======
                 ids += 1
                 p = random.choice(i[1])
                 if(p[1] == 'AB'):
                     population_lab.append([years[l][0], i[0], p[0], random.choice(['A', 'B']), random.choice(days), random.choice(meettime[1]),  ids, -1])
                 else:
-                    population_lab.append([years[l][0], i[0], p[0], p[1], random.choice(days), random.choice(meettime[1]),  ids, -1])    
->>>>>>> 681e808d5312b55001a8966530a1a586fdef0e15
+                    population_lab.append([years[l][0], i[0], p[0], p[1], random.choice(days), random.choice(meettime[1]),  ids, -1])
 
     population.append(population_sub)
     population.append(population_lab)
     return population
 
-<<<<<<< HEAD
-s = create_population()
-=======
-
-st = create_population()
+population = create_population()
 def sortIt(population):
     population[0].sort(key = lambda x:x[-1])
     population[1].sort(key = lambda x:x[-1])
@@ -77,18 +51,18 @@ def fitness(population):
         for j in meettime[1]:
             # print (k + j)
             count_tea_conflict = Counter(i[2] for i in population[1] if i[4] == k and i[5] == j)
-            # extracting from counter dictionary                  
+            # extracting from counter dictionary
             for i in population[1]:
                 for key, value in count_tea_conflict.items():
                     if (key == i[2] and i[4] == k and i[5] == j):
                         i[-1] += value
-    
-    # -----------Conflicts for same teachers in different labs and occupying same room at same day and time 
+
+    # -----------Conflicts for same teachers in different labs and occupying same room at same day and time
     for k in days:
         for j in meettime[0]:
             count_tea_conflict = Counter(i[2] for i in population[0] if i[5] == k and i[6] == j)
             count_room_conflict = Counter(i[4] for i in population[0] if i[5] == k and i[6] == j)
-            # extracting from counter dictionary                  
+            # extracting from counter dictionary
             for i in population[0]:
                 for key, value in count_tea_conflict.items():
                     if key == i[2] and i[5] == k and i[6] == j:
@@ -97,14 +71,14 @@ def fitness(population):
                     if key == i[4] and i[5] == k and i[6] == j:
                         i[-1] += value
 
-    # ----------- Conflicts btwn lab and theory sub at same time 
+    # ----------- Conflicts btwn lab and theory sub at same time
     for d in days:
         for i in meettime[1]:
             count_sub = 0
-            count_lab = 0    
-            for s in population[1]: 
+            count_lab = 0
+            for s in population[1]:
                 if s[5] == i and s[4] == d:
-                    count_lab += 1 
+                    count_lab += 1
                     if(count_sub > 0):
                         s[-1] += 1
             for s in population[0]:
@@ -117,7 +91,10 @@ def fitness(population):
                 if s[4] == d and s[5] == i:
                     s[-1] += 1
     return population
-    
+
+st = fitness(population)
+population = st
+
 def check(lists):
     for i in lists:
         if i < 5:
@@ -147,13 +124,17 @@ def labs_labs(population):
                 for k in population[1]:
                     if(k[4] == d and k[5] == m):
                         k[-1] = k[-1] + sum(lists) - 2
-    return population  
+    return population
+
+st = labs_labs(population)
+population = st
+
 
 def tournament(population):
-    tournment_size_sub = len(population[0]) / 10
-    tournment_size_lab = len(population[1]) / 10
-    newpop_sub_size = len(population[0]) / 4
-    newpop_lab_size = len(population[1]) / 4
+    tournment_size_sub = len(population[0]) // 10
+    tournment_size_lab = len(population[1]) // 10
+    newpop_sub_size = len(population[0]) // 4
+    newpop_lab_size = len(population[1]) // 4
     # some max no.
     cross = []
     # algo
@@ -183,6 +164,8 @@ def tournament(population):
 
     return population1
 
+st = tournament(population)
+population = st
 def swap(a, b):
     a, b = b, a
 
@@ -190,7 +173,7 @@ def crossover(selection):
     pc = 0.57
     population_lab_size = len(selection[1])
     population_sub_size = len(selection[0])
-    
+
     # for lectures
     for i in range(0, population_sub_size - 1 , 2):
         if random.random() > pc:
@@ -209,6 +192,9 @@ def crossover(selection):
             if(random.choice([True, False])):
                 swap(selection[1][i][5], selection[1][i+1][5])# days
     return selection
+
+st = crossover(population)
+population = st
 
 def mutation(population1):
     pm = 0.2
@@ -230,50 +216,108 @@ def mutation(population1):
                 swap(population1[1][i][5], random.choice(days))# days
             if(random.choice([True, False])):
                 swap(population1[1][i][6], random.choice(meettime[1]))# slots
-                
+
     return population1
+
+st = mutation(population)
+population = st
+
 
 def new_population(population, population1):
     # new pop will be 30% of original => 25% from tournament selection + 5% random choice
-    per_sub = len(population[0]) / 20
-    per_lab = len(population[1]) / 20 
+    per_sub = len(population[0]) // 20
+    per_lab = len(population[1]) // 20
     for i in range(per_sub):
         population1[0].append(random.choice(population[0]))
     for i in range(per_lab):
-        population1[1].append(random.choice(population[0]))
+        population1[1].append(random.choice(population[1]))
 
     population = population1
     return population
 
+st = crossover(population)
+population = st
+for x in st[0]:
+    print(x)
+for x in st[1]:
+    print(x)
+
 def change_fitness(population):
     for x in population:
         for j in x:
-            j[5] = 0
+            j[-1] = 0
     return population
 
+def timetables(population):
+    timetable = [ [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]] ]
+    lab_matrix = [[2,2,1,1],[2,2,1,1],[2,2,1,1],[2,2,1,1],[2,2,1,1]]
+    tea_matrix = [ [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]] ]
+    # population[0].sort(key = lambda x:x[-1])
+    population[0].sort(key = lambda x:x[-1])
+    population[1].sort(key = lambda x:x[-1])
 
+    for y in years:
+        for div in divs:
+            y = years[0]
+            div = 'A'
+            timetable = [ [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]] ]
+            toplabtime = []
+            toplabs =  [[],[],[],[]]
+            count_toplabs = 0
+            for i in population[1]:
+                if i[0] == y[0] and i[3] == div:
+                    if(lab_matrix[days.index(i[4])][meettime[1].index(i[5])]>0):
+                        if(i[5] and i[4] not in toplabtime):
+                            if all([i[4] not in item for item in toplabtime]):
+                                if len(toplabtime) <= len(y[2]):
+                                    toplabtime.append(i[4]+i[5])
+                                    if len(toplabtime) >= len(y[2]):
+                                        break
+            count_toplabs = 0
+            for i in population[1]:
+                if i[0] == y[0] and i[3] == div:
+                    if i[4]+i[5] in toplabtime:
+                        index1 = toplabtime.index(i[4]+i[5])
+                        if len(toplabs[index1]) < 4:
+                            if i[-2] not in toplabs[index1]:
+                                count_toplabs +=1
+                                toplabs[index1].append(i[-2])
+                                timetable[days.index(i[4])][meettime[1].index(i[5])*2].append(i[-2])
+                                # for conflicts in last 2 lectures in tt
+                                if(not meettime[1][-1]==i[5]):
+                                    timetable[days.index(i[4])][meettime[1].index(i[5])*2+1].append(i[-2])
+                                else:
+                                    timetable[days.index(i[4])][5].append(i[-2])
+                                # if count_toplabs >= 16:
+                                #     break
+            for k in toplabtime:
+                lab_matrix[days.index(k[:3])][meettime[1].index(k[3:])]-=1
+            # for subjects
+            for d in days:
+                for m in meettime[0]:
+                    for i in population[0]:
+                        if i[0] == y[0] and i[3] == 'A':
+                            if i[5] == d and i[6] == m:
+                                if not timetable[days.index(d)][meettime[0].index(m)]:
+                                    if i[2] not in tea_matrix[days.index(d)][meettime[0].index(m)]:
+                                        timetable[days.index(d)][meettime[0].index(m)].append(i[-2])
+                                        tea_matrix[days.index(d)][meettime[0].index(m)].append(i[1])
+
+    return timetable
 
 if __name__ == '__main__':
     population = create_population()
-    # print(population)
     population = fitness(population)
-    # print(population[1])
-    s = sortIt(population)
-    print(s[1])
-
-    # population = labs_labs(population)
-    # population1 = tournament(population)
-    # population1 = crossover(population1)
-    # population1 = mutation(population1)
-    # population1 = change_fitness(population1)
-    # population1 = fitness(population1)
-    # population = new_population(population, population1)
-
-# pop = create_population()
-# pop = fitness(pop)
-# pop = labs_labs(pop[1])
-# pop1 = tournament(pop)
-
-# print(len(pop1[0]))
-# print(pop[0])
->>>>>>> 681e808d5312b55001a8966530a1a586fdef0e15
+    population = labs_labs(population)
+    population1 = tournament(population)
+    population1 = crossover(population1)
+    population1 = mutation(population1)
+    population1 = change_fitness(population1)
+    population1 = fitness(population1)
+    population = new_population(population, population1)
+    print(population)
+    for x in population[1]:
+        if x[0] == 'se' and x[3] == 'A':
+            print(x)
+    timetable = timetables(population)
+    print(timetable)
--- mains.py
+++ mains.py
@@ -1,126 +1,296 @@
-from data import course, days, meettime
+from data import subjects, days, meettime, rooms, labs, years, teachers
 from collections import Counter
 import random
 import sys
 
-population_size = 200
+population_size_sub = 100
+population_size_lab = 50
 
 def create_population():
     population = []
-    l = 0
-    population_size = 200
-    for j in range(population_size):
-        for i in course:
-            population.append([l , i[0], random.choice(i[2]), random.choice(days), random.choice(meettime), 0])
-            l = l + 1
+    population_sub = []
+    population_lab = []
+    for j in range(population_size_sub):
+        l = -1
+        ids = 0
+        for k in subjects:
+            l += 1
+            for i in k:
+                ids += 1
+                p = random.choice(i[1])
+                if(p[1] == 'AB'):
+                    population_sub.append([years[l][0], i[0], p[0], random.choice(['A', 'B']), random.choice(rooms), random.choice(days), random.choice(meettime[0]), ids,-1])
+                else:
+                    population_sub.append([years[l][0], i[0], p[0], p[1], random.choice(rooms), random.choice(days), random.choice(meettime[0]), ids, -1])
+    for j in range(population_size_lab):
+        l = -1
+        for k in labs:
+            l += 1
+            for i in k:
+                ids += 1
+                p = random.choice(i[1])
+                if(p[1] == 'AB'):
+                    population_lab.append([years[l][0], i[0], p[0], random.choice(['A', 'B']), random.choice(days), random.choice(meettime[1]),  ids, -1])
+                else:
+                    population_lab.append([years[l][0], i[0], p[0], p[1], random.choice(days), random.choice(meettime[1]),  ids, -1])
+
+    population.append(population_sub)
+    population.append(population_lab)
+    return population
+
+
+st = create_population()
+def sortIt(population):
+    population[0].sort(key = lambda x:x[-1])
+    population[1].sort(key = lambda x:x[-1])
     return population
 
 def fitness(population):
-    count = 0
-    for i in days:
-        for j in meettime:
-            cou = Counter(population[s][2] for s in range(len(population)) if population[s][4] == j and population[s][3] == i )
+    # -----------Conflicts for same teachers in different labs at same day and time
+    for k in days:
+        for j in meettime[1]:
+            # print (k + j)
+            count_tea_conflict = Counter(i[2] for i in population[1] if i[4] == k and i[5] == j)
+            # extracting from counter dictionary
+            for i in population[1]:
+                for key, value in count_tea_conflict.items():
+                    if (key == i[2] and i[4] == k and i[5] == j):
+                        i[-1] += value
 
-    # extracting from counter dictionary
-    for i in population:
-        for j,k in cou.items():
-            if j == i[2]:
-                i[5] = k
+    # -----------Conflicts for same teachers in different labs and occupying same room at same day and time
+    for k in days:
+        for j in meettime[0]:
+            count_tea_conflict = Counter(i[2] for i in population[0] if i[5] == k and i[6] == j)
+            count_room_conflict = Counter(i[4] for i in population[0] if i[5] == k and i[6] == j)
+            # extracting from counter dictionary
+            for i in population[0]:
+                for key, value in count_tea_conflict.items():
+                    if key == i[2] and i[5] == k and i[6] == j:
+                        i[-1] += value
+                for key, value in count_room_conflict.items():
+                    if key == i[4] and i[5] == k and i[6] == j:
+                        i[-1] += value
+
+    # ----------- Conflicts btwn lab and theory sub at same time
+    for d in days:
+        for i in meettime[1]:
+            count_sub = 0
+            count_lab = 0
+            for s in population[1]:
+                if s[5] == i and s[4] == d:
+                    count_lab += 1
+                    if(count_sub > 0):
+                        s[-1] += 1
+            for s in population[0]:
+                # print(i[6] +i[7])
+                if (s[5] == d and int(i[0]+i[1]) <= int(s[6][0]+s[6][1]) < int(i[5]+i[6])) :
+                    count_sub += 1
+                    if(count_lab > 0):
+                        s[-1] += 1
+            for s in population[1]:
+                if s[4] == d and s[5] == i:
+                    s[-1] += 1
     return population
 
+def check(lists):
+    for i in lists:
+        if i < 5:
+            return True
+        return False
 
+def labs_labs(population):
+    lists = [0, 0, 0, 0, 0, 0]
+    for d in days:
+        for m in meettime[1]:
+            for j in population[1]:
+                if(j[4] == d and j[5] == m):
+                    for y in range(len(years)):
+                        if(years[y][0] == j[0]):
+                            break
+                        if(j[3] == 'A'):
+                            lists[y*2]+=1
+                        else:
+                            lists[y*2+1]+=1
+            flag = check(lists)
+            sums = 0
+            if(flag == True):
+                sums = sum(lists) - 2
+                if(sums<3):
+                    no = "no problem"
+            else:
+                for k in population[1]:
+                    if(k[4] == d and k[5] == m):
+                        k[-1] = k[-1] + sum(lists) - 2
+    return population
 
-# population size = 200
-# initial population = 1400
-# tournment size e= 25% i.e., 350
-# at the end new population contains 200 items
-# some max no.
-# algo
-# iterate 200 time
-# for each iteration choose 350 random elements from overall population
 def tournament(population):
-    tournment_size = 350
+    tournment_size_sub = len(population[0]) // 10
+    tournment_size_lab = len(population[1]) // 10
+    newpop_sub_size = len(population[0]) // 4
+    newpop_lab_size = len(population[1]) // 4
     # some max no.
     cross = []
     # algo
     # iterate 200 time
     # for each iteration choose 350 random elements from overall population
     population1 = []
-    for i in range(population_size):
+    population1_sub = []
+    population1_lab = []
+    for i in range(newpop_sub_size):
         min = sys.maxsize
-        for j in range(tournment_size):
-            cross = random.choice(population)
+        for j in range(tournment_size_sub):
+            cross = random.choice(population[0])
             # print(len(cross))
-            if(min > cross[5]):
-                min = cross[5]
-        population1.append(cross)
+            if(min > cross[-1]):
+                min = cross[-1]
+        population1_sub.append(cross)
+    for i in range(newpop_lab_size):
+        min = sys.maxsize
+        for j in range(tournment_size_lab):
+            cross = random.choice(population[1])
+            # print(len(cross))
+            if(min > cross[-1]):
+                min = cross[-1]
+        population1_lab.append(cross)
+    population1.append(population1_sub)
+    population1.append(population1_lab)
+
     return population1
 
 def swap(a, b):
-    temp = a
-    a = b
-    b = temp
-    return a, b
+    a, b = b, a
 
-def crossover(population1):
-    pc = 0.7
-    # here selection is list obtained after tournament selection
-    for i in range(0, 200, 2):
+def crossover(selection):
+    pc = 0.57
+    population_lab_size = len(selection[1])
+    population_sub_size = len(selection[0])
+
+    # for lectures
+    for i in range(0, population_sub_size - 1 , 2):
         if random.random() > pc:
             if(random.choice([True, False])):
-                swap(population1[i][4], population1[i+1][4])
+                swap(selection[0][i][4], selection[0][i+1][4])# rooms
             if(random.choice([True, False])):
-                swap(population1[i][5], population1[i+1][5])
-    return population1
+                swap(selection[0][i][6], selection[0][i+1][6])# slot
+            if(random.choice([True, False])):
+                swap(selection[0][i][5], selection[0][i+1][5])# days
+
+    # for labs
+    for i in range(0, population_lab_size -1, 2):
+        if random.random() > pc:
+            if(random.choice([True, False])):
+                swap(selection[1][i][6], selection[1][i+1][6])# slot
+            if(random.choice([True, False])):
+                swap(selection[1][i][5], selection[1][i+1][5])# days
+    return selection
 
 def mutation(population1):
     pm = 0.2
-    for i in range(0, 200, 2):
+    population_lab_size = len(population1[1])
+    population_sub_size = len(population1[0])
+    for i in range(0, population_sub_size, 2):
         if random.random() < pm:
             if(random.choice([True, False])):
-                swap(population1[i][4], random.choice(days))
+                swap(population1[0][i][4], random.choice(rooms))# rooms
             if(random.choice([True, False])):
-                swap(population1[i][5], random.choice(meettime))
+                swap(population1[0][i][6], random.choice(meettime[0]))# slots
+            if(random.choice([True, False])):
+                swap(population1[0][i][5], random.choice(days))# days
+
+    pm = 0.2
+    for i in range(0, population_lab_size - 1, 2):
+        if random.random() < pm:
+            if(random.choice([True, False])):
+                swap(population1[1][i][5], random.choice(days))# days
+            if(random.choice([True, False])):
+                swap(population1[1][i][6], random.choice(meettime[1]))# slots
+
     return population1
 
 def new_population(population, population1):
-    # create new population .. here range for new population should be 25% of original ..so 200 from crossover and mutation + 150
-    for i in range(150):
-        population1.append(random.choice(population))
+    # new pop will be 30% of original => 25% from tournament selection + 5% random choice
+    per_sub = len(population[0]) // 20
+    per_lab = len(population[1]) // 20
+    for i in range(per_sub):
+        population1[0].append(random.choice(population[0]))
+    for i in range(per_lab):
+        population1[1].append(random.choice(population[0]))
+
     population = population1
     return population
 
-def timetables(population):
-    timetable = []
-    for i in days:
-        for j in meettime:
-            min = sys.maxsize
-            for s in range(len(population)):
-                table = population[s]
-                if(j==table[4] and i==table[3] and table[5]<min):
-                    min = table[5]
-                    remem = table
-            timetable.append(remem)
-    return timetable
-
 def change_fitness(population):
     for x in population:
-        x[5] = 0
+        for j in x:
+            j[-1] = 0
     return population
+
+def timetables(population):
+    divs = ['A', 'B']
+    toplabtime = []
+    toplabs =  [[],[],[],[]]
+    count_toplabs = 0
+    timetable = [ [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]] ]
+    lab_matrix = [[2,2,1,1],[2,2,1,1],[2,2,1,1],[2,2,1,1],[2,2,1,1]]
+    tea_matrix = [ [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]], [[],[],[],[],[],[],[]] ]
+    # population[0].sort(key = lambda x:x[-1])
+    # print(population[1])
+    population[0].sort(key = lambda x:x[-1])
+    population[1].sort(key = lambda x:x[-1])
+    # print(population[1])
+    for y in years:
+        for div in divs:
+            for i in population[1]:
+                if i[0] == y[0] and i[3] == div:
+                    if(lab_matrix[days.index(i[4])][meettime[1].index(i[5])]>0):
+                        if(i[5] and i[4] not in toplabtime):
+                            if all([i[4] not in item for item in toplabtime]):
+                                if len(toplabtime) <= len(y[2]):
+                                    toplabtime.append(i[4]+i[5])
+                                    if len(toplabtime) >= len(y[2]):
+                                        break
+            for i in population[1]:
+                if i[0] == y[0] and i[3] == div:
+                    if i[4]+i[5] in toplabtime:
+                        index1 = toplabtime.index(i[4]+i[5])
+                        print(toplabtime)
+                        print(index1)
+                        if len(toplabs[index1]) < 4:
+                            if i[-2] not in toplabs[index1]:
+                                count_toplabs +=1
+                                toplabs[index1].append(i[-2])
+                                timetable[days.index(i[4])][meettime[1].index(i[5])*2].append(i[-2])
+                                # for conflicts in last 2 lectures in tt
+                                if(not meettime[1][-1]==i[5]):
+                                    timetable[days.index(i[4])][meettime[1].index(i[5])*2+1].append(i[-2])
+                                else:
+                                    timetable[days.index(i[4])][5].append(i[-2])
+                                if count_toplabs >= 12:
+                                    break
+            for k in toplabtime:
+                lab_matrix[days.index(k[:3])][meettime[1].index(k[3:])]-=1
+
+            # for subjects
+            for d in days:
+                for m in meettime[0]:
+                    for i in population[0]:
+                        if i[5] == d and i[6] == m:
+                            if not timetable[days.index(d)][meettime[0].index(m)]:
+                                if i[2] not in tea_matrix[days.index(d)][meettime[0].index(m)]:
+                                    timetable[days.index(d)][meettime[0].index(m)].append(i[-2])
+                                    tea_matrix[days.index(d)][meettime[0].index(m)].append(i[2])
+    return timetable
 
 if __name__ == '__main__':
     population = create_population()
-    for i in range(100):
-        population = fitness(population)
-        population1 = tournament(population)
-        population1 = crossover(population1)
-        population1 = mutation(population1)
-        population1 = change_fitness(population1)
-        population1 = fitness(population1)
-        population = new_population(population, population1)
-        timetable = timetables(population)
-        timetable = change_fitness(timetable)
-        timetable = fitness(timetable)
-
-    for u in timetable:
-        print(u)
+    population = fitness(population)
+    population = labs_labs(population)
+    population1 = tournament(population)
+    population1 = crossover(population1)
+    population1 = mutation(population1)
+    population1 = change_fitness(population1)
+    population1 = fitness(population1)
+    # print((population[1]))
+    population = new_population(population, population1)
+    timetable = timetables(population)
+    print(timetable)
