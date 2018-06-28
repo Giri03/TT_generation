0bad39c23082dc49035f52eb32e27f562102f881
Giri03 <girisha.navani97@gmail.com>
2018-06-28 09:34:20 +0530

    change in main file

--- main.py
+++ main.py
@@ -80,12 +80,42 @@ def fitness(population):
             for s in population[1]:
                 if s[4] == d and s[5] == i:
                     s[-1] += 1
+    return population
+    
+def check(lists):
+    for i in lists:
+        if i < 5:
+            return True
+        return False
 
-    return population    
+def labs_labs(population):
+    lists = [0, 0, 0, 0, 0, 0]
+    for d in days:
+        for m in meettime[1]:
+            for j in population[1]:
+                if(j[4] == d and j[5] == m):
+                    for y in range(len(years)):
+                        if(years[y][0] == j[0]):
+                            break
+                        if(j[3] == 'A'):
+                            lists[y*2]+=1
+                        else:
+                            lists[y*2+1]+=1
+            flag = check(lists)
+            sums = 0
+            if(flag == True):
+                sums = sum(lists) - 2
+                if(sums<3):
+                    no = "no problem"
+            else:
+                for k in population[1]:
+                    if(k[4] == d and k[5] == m):
+                        k[-1] = k[-1] + sum(lists) - 2
+    return population  
 
 def tournament(population):
     tournment_size_sub = len(population[0]) / 10
-    tournment_size_lab = len(population[0]) / 10
+    tournment_size_lab = len(population[1]) / 10
     newpop_sub_size = len(population[0]) / 4
     newpop_lab_size = len(population[1]) / 4
     # some max no.
@@ -117,13 +147,67 @@ def tournament(population):
 
     return population1
 
+def swap(a, b):
+    a, b = b, a
+
+def crossover(selection):
+    pc = 0.57
+    population_lab_size = len(selection[1])
+    population_sub_size = len(selection[0])
+    print(population_lab_size )
+    print(population_sub_size)
+    print(selection[0][337][4])
+    print('#########')
+    # for lectures
+    for i in range(0, population_sub_size - 1 , 2):
+        if random.random() > pc:
+            if(random.choice([True, False])):
+                print(i)
+                swap(selection[0][i][4], selection[0][i+1][4])# rooms
+            if(random.choice([True, False])):
+                swap(selection[0][i][6], selection[0][i+1][6])# slot
+            if(random.choice([True, False])):
+                swap(selection[0][i][5], selection[0][i+1][5])# days
+
+    # for labs
+    for i in range(0, population_lab_size -1, 2):
+        if random.random() > pc:
+            if(random.choice([True, False])):
+                swap(selection[1][i][6], selection[1][i+1][6])# slot
+            if(random.choice([True, False])):
+                swap(selection[1][i][5], selection[1][i+1][5])# days
+    return selection
+
+def mutation(population1):
+    pm = 0.2
+    population_lab_size = len(population1[1])
+    population_sub_size = len(population1[0])
+    for i in range(0, population_sub_size, 2):
+        if random.random() < pm:
+            if(random.choice([True, False])):
+                swap(population1[0][i][4], random.choice(rooms))# rooms
+            if(random.choice([True, False])):
+                swap(population1[0][i][6], random.choice(meettime[0]))# slots
+            if(random.choice([True, False])):
+                swap(population1[0][i][5], random.choice(days))# days
+
+    pm = 0.2
+    for i in range(0, population_lab_size - 1, 2):
+        if random.random() < pm:
+            if(random.choice([True, False])):
+                swap(population1[1][i][5], random.choice(days))# days
+            if(random.choice([True, False])):
+                swap(population1[1][i][6], random.choice(meettime[1]))# slots
+                
+    return population1
+
 def new_population(population, population1):
     # new pop will be 30% of original => 25% from tournament selection + 5% random choice
-    5_per_sub = len(population[0])/20
-    5_per_lab = len(population[1])/20 
-    for i in range(5_per_sub):
+    per_sub = len(population[0]) / 20
+    per_lab = len(population[1]) / 20 
+    for i in range(per_sub):
         population1[0].append(random.choice(population[0]))
-    for i in range(5_per_sub):
+    for i in range(per_lab):
         population1[1].append(random.choice(population[0]))
 
     population = population1
@@ -135,10 +219,23 @@ def change_fitness(population):
             j[5] = 0
     return population
 
+if __name__ == '__main__':
+    population = create_population()
+    # print(population)
+    population = fitness(population)
+    population = labs_labs(population)
+    population1 = tournament(population)
+    population1 = crossover(population1)
+    population1 = mutation(population1)
+    print(population1)
+    population1 = change_fitness(population1)
+    population1 = fitness(population1)
+    population = new_population(population, population1)
 
-pop = create_population()
-pop = fitness(pop)
-print(pop[0])
-pop1 = tournament(pop)
+# pop = create_population()
+# pop = fitness(pop)
+# pop = labs_labs(pop[1])
+# pop1 = tournament(pop)
+
 # print(len(pop1[0]))
 # print(pop[0])
